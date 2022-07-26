a74fa2c52a76cb70a8c0512918420e3ab30fc2db
Gmatieso <matiesogeoffrey@gmail.com>
2022-07-27 01:14:41 +0300

    Created FilterContent data class

---
+++ app/src/main/java/com/example/dayoneofcomposeui/ui/theme/utils/FilterContent.kt
@@ -0,0 +1,11 @@
+package com.example.dayoneofcomposeui.ui.theme.utils
+
+import androidx.compose.ui.graphics.Color
+/*
+This data class takes in contentColor, backgroundColor and filterText as its parameter
+ */
+data class FilterContent(
+    val contentColor: Color,
+    val backgroundColor: Color,
+    val filterText: String
+)
