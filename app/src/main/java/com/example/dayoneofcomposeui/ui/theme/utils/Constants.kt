32d0f026825c31fbb75f6b2c0c3f5142b08152a7
Gmatieso <matiesogeoffrey@gmail.com>
2022-07-27 01:18:29 +0300

    Created Filter Content List variable that take listOf FilterContent

--- app/src/main/java/com/example/dayoneofcomposeui/ui/theme/utils/Constants.kt
+++ app/src/main/java/com/example/dayoneofcomposeui/ui/theme/utils/Constants.kt
@@ -1,2 +1,17 @@
 package com.example.dayoneofcomposeui.ui.theme.utils
 
+import androidx.compose.ui.graphics.Color
+import com.example.dayoneofcomposeui.ui.theme.Black
+
+val FILTER_CONTENT_LIST =  listOf(
+   // put your content here
+    FilterContent(Color.White, Black, "Mood swings"),
+    FilterContent(Black, Color.White, "Stress"),
+    FilterContent(Black, Color.White, "Depression"),
+    FilterContent(Black, Color.White, "Anxiety"),
+    FilterContent(Black, Color.White, "Anger"),
+    FilterContent(Black, Color.White, "Excitement"),
+    FilterContent(Black, Color.White, "Fear"),
+    FilterContent(Black, Color.White, "Joy"),
+    FilterContent(Black, Color.White, "Horror")
+)
